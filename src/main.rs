7703644715e19dc5a590094bfd9e3ef87b1f3825
ChillyForest <1394466835@qq.com>
2023-12-30 06:42:39 +0800

    feat: walking a map

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,9 @@ impl GameState for State {
         self.run_systems();
         player_input(self, ctx);
 
+        let map = self.ecs.fetch::<Vec<TitleType>>();
+        draw_map(&map, ctx);
+
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
@@ -65,10 +68,14 @@ struct Player {}
 fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
     let mut positions = ecs.write_storage::<Position>();
     let mut players = ecs.write_storage::<Player>();
+    let map = ecs.fetch::<Vec<TitleType>>();
 
     for (_player, pos) in (&mut players, &mut positions).join() {
-        pos.x = min(79, max(0, pos.x + delta_x));
-        pos.y = min(49, max(0, pos.y + delta_y));
+        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
+        if map[destination_idx] != TitleType::Wall {
+            pos.x = min(79, max(0, pos.x + delta_x));
+            pos.y = min(49, max(0, pos.y + delta_y));
+        }
     }
 }
 
@@ -86,6 +93,74 @@ fn player_input(gs: &mut State, ctx: &Rltk) {
     }
 }
 
+#[derive(PartialEq, Clone, Copy)]
+enum TitleType {
+    Wall,
+    Floor,
+}
+
+pub fn xy_idx(x: i32, y: i32) -> usize {
+    (y as usize * 80) + x as usize
+}
+
+fn new_map() -> Vec<TitleType> {
+    let mut map = vec![TitleType::Floor; 80 * 50];
+
+    for x in 0..80 {
+        map[xy_idx(x, 0)] = TitleType::Wall;
+        map[xy_idx(x, 49)] = TitleType::Wall;
+    }
+
+    for y in 0..50 {
+        map[xy_idx(0, y)] = TitleType::Wall;
+        map[xy_idx(79, y)] = TitleType::Wall;
+    }
+
+    let mut rng = rltk::RandomNumberGenerator::new();
+    for _i in 0..400 {
+        let x = rng.roll_dice(1, 79);
+        let y = rng.roll_dice(1, 49);
+        let idx = xy_idx(x, y);
+        if idx != xy_idx(40, 25) {
+            map[idx] = TitleType::Wall;
+        }
+    }
+
+    map
+}
+
+fn draw_map(map: &[TitleType], ctx: &mut Rltk) {
+    let mut y = 0;
+    let mut x = 0;
+    for title in map.iter() {
+        match title {
+            TitleType::Wall => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.0, 1.0, 0.0),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('#'),
+                );
+            }
+            TitleType::Floor => {
+                ctx.set(
+                    x,
+                    y,
+                    RGB::from_f32(0.5, 0.5, 0.5),
+                    RGB::from_f32(0., 0., 0.),
+                    rltk::to_cp437('.'),
+                );
+            }
+        }
+        x += 1;
+        if x > 79 {
+            x = 0;
+            y += 1;
+        }
+    }
+}
+
 fn main() {
     let context = RltkBuilder::simple80x50()
         .with_title("Rougelike Tutorial")
@@ -98,6 +173,8 @@ fn main() {
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
 
+    gs.ecs.insert(new_map());
+
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -108,19 +185,6 @@ fn main() {
         })
         .with(Player {})
         .build();
-
-    for i in 0..10 {
-        gs.ecs
-            .create_entity()
-            .with(Position { x: i * 7, y: 20 })
-            .with(Renderable {
-                glyph: rltk::to_cp437('☺'),
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(LeftMover {})
-            .build();
-    }
 
     let _ = rltk::main_loop(context, gs);
 }
