4b24f183a786d235adbbfab693e5e7114af35ef0
ChillyForest <1394466835@qq.com>
2023-12-30 08:58:13 +0800

    feat: more map

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 extern crate rltk;
-use rltk::{GameState, Rltk, RltkBuilder, VirtualKeyCode, RGB};
+use rltk::{GameState, Rltk, RltkBuilder, RGB};
 use specs::prelude::*;
-use specs_derive::Component;
-use std::cmp::{max, min};
 
-struct State {
-    ecs: World,
+mod components;
+pub use components::*;
+mod rect;
+pub use rect::Rect;
+mod map;
+pub use map::*;
+mod player;
+pub use player::*;
+
+pub struct State {
+    pub ecs: World,
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        self.ecs.maintain();
+    }
 }
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
+
         self.run_systems();
         player_input(self, ctx);
 
@@ -25,143 +39,8 @@ impl GameState for State {
         }
     }
 }
-impl State {
-    fn run_systems(&mut self) {
-        let mut lw = LeftMover {};
-        lw.run_now(&self.ecs);
-        self.ecs.maintain();
-    }
-}
 
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: rltk::FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-#[derive(Component)]
-struct LeftMover {}
-
-impl<'a> System<'a> for LeftMover {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_lefty, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
-
-#[derive(Component)]
-struct Player {}
-
-fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
-    let mut positions = ecs.write_storage::<Position>();
-    let mut players = ecs.write_storage::<Player>();
-    let map = ecs.fetch::<Vec<TitleType>>();
-
-    for (_player, pos) in (&mut players, &mut positions).join() {
-        let destination_idx = xy_idx(pos.x + delta_x, pos.y + delta_y);
-        if map[destination_idx] != TitleType::Wall {
-            pos.x = min(79, max(0, pos.x + delta_x));
-            pos.y = min(49, max(0, pos.y + delta_y));
-        }
-    }
-}
-
-fn player_input(gs: &mut State, ctx: &Rltk) {
-    // Player movement
-    match ctx.key {
-        Some(key) => match key {
-            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
-            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
-            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
-            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
-            _ => {}
-        },
-        None => {}
-    }
-}
-
-#[derive(PartialEq, Clone, Copy)]
-enum TitleType {
-    Wall,
-    Floor,
-}
-
-pub fn xy_idx(x: i32, y: i32) -> usize {
-    (y as usize * 80) + x as usize
-}
-
-fn new_map() -> Vec<TitleType> {
-    let mut map = vec![TitleType::Floor; 80 * 50];
-
-    for x in 0..80 {
-        map[xy_idx(x, 0)] = TitleType::Wall;
-        map[xy_idx(x, 49)] = TitleType::Wall;
-    }
-
-    for y in 0..50 {
-        map[xy_idx(0, y)] = TitleType::Wall;
-        map[xy_idx(79, y)] = TitleType::Wall;
-    }
-
-    let mut rng = rltk::RandomNumberGenerator::new();
-    for _i in 0..400 {
-        let x = rng.roll_dice(1, 79);
-        let y = rng.roll_dice(1, 49);
-        let idx = xy_idx(x, y);
-        if idx != xy_idx(40, 25) {
-            map[idx] = TitleType::Wall;
-        }
-    }
-
-    map
-}
-
-fn draw_map(map: &[TitleType], ctx: &mut Rltk) {
-    let mut y = 0;
-    let mut x = 0;
-    for title in map.iter() {
-        match title {
-            TitleType::Wall => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.0, 1.0, 0.0),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('#'),
-                );
-            }
-            TitleType::Floor => {
-                ctx.set(
-                    x,
-                    y,
-                    RGB::from_f32(0.5, 0.5, 0.5),
-                    RGB::from_f32(0., 0., 0.),
-                    rltk::to_cp437('.'),
-                );
-            }
-        }
-        x += 1;
-        if x > 79 {
-            x = 0;
-            y += 1;
-        }
-    }
-}
-
-fn main() {
+fn main() -> rltk::BError {
     let context = RltkBuilder::simple80x50()
         .with_title("Rougelike Tutorial")
         .build()
@@ -173,11 +52,16 @@ fn main() {
     gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
 
-    gs.ecs.insert(new_map());
+    let (rooms, map) = new_map_rooms_and_corridors();
+    gs.ecs.insert(map);
+    let (player_x, player_y) = rooms[0].center();
 
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position {
+            x: player_x,
+            y: player_y,
+        })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
@@ -186,5 +70,5 @@ fn main() {
         .with(Player {})
         .build();
 
-    let _ = rltk::main_loop(context, gs);
+    rltk::main_loop(context, gs)
 }
