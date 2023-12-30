380dea39d16451e6eab9dd2d59abe5dec2eb207d
ChillyForest <1394466835@qq.com>
2023-12-30 11:11:14 +0800

    feat: add monsters

--- src/player.rs
+++ src/player.rs
@@ -1,5 +1,5 @@
-use super::{Map, Player, Position, State, TitleType, Viewshed};
-use rltk::{Rltk, VirtualKeyCode};
+use super::{Map, Player, Position, RunState, State, TitleType, Viewshed};
+use rltk::{Point, Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use std::cmp::{max, min};
 
@@ -14,32 +14,41 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
         if map.titles[destination_idx] != TitleType::Wall {
             pos.x = min(79, max(0, pos.x + delta_x));
             pos.y = min(49, max(0, pos.y + delta_y));
+
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
             viewshed.dirty = true;
         }
     }
 }
 
-pub fn player_input(gs: &mut State, ctx: &Rltk) {
+pub fn player_input(gs: &mut State, ctx: &Rltk) -> RunState {
     // Player movement
     match ctx.key {
         Some(key) => match key {
             VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
-                try_move_player(-1, 0, &mut gs.ecs)
+                try_move_player(-1, 0, &mut gs.ecs);
             }
 
             VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
-                try_move_player(1, 0, &mut gs.ecs)
+                try_move_player(1, 0, &mut gs.ecs);
             }
 
             VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
-                try_move_player(0, -1, &mut gs.ecs)
+                try_move_player(0, -1, &mut gs.ecs);
             }
 
             VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
                 try_move_player(0, 1, &mut gs.ecs)
             }
-            _ => {}
+            _ => {
+                return RunState::Paused;
+            }
         },
-        None => {}
+        None => {
+            return RunState::Paused;
+        }
     }
+    RunState::Running
 }
